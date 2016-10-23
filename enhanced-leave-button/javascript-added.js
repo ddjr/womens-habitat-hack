5d343eea1c92936ac98e9a360890aec62c827a15
Bill Ted <mhoch2@illinois.edu>
2016-10-23 10:11:54 -0500

    Update javascript-added.js

--- enhanced-leave-button/javascript-added.js
+++ enhanced-leave-button/javascript-added.js
@@ -1,9 +1,10 @@
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script type="text/javascript">
 function leave() {
-  // Get away right now
+  // Get away right now by opening a new tab with prepopulated google
   window.open("https://www.google.ca/?gfe_rd=cr&ei=YMYKWKraGsah8we984CwBQ#q=weather+toronto", "_newtab");
-  // Replace current site with another benign site
+  // Replace current site with another benign site -- takes slower to load
+  //window.location.replace will cause the back-button history to not be there
   window.location.replace('http://www.nbc.com/schedule');
 }
 
